0c3584f0737c11da2a21a53523689703aef5fd13
colg8 <129955515+colg8git@users.noreply.github.com>
2024-03-03 15:47:30 +0200

    pub

--- source/home/index.js
+++ source/home/index.js
@@ -1,11 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     const fileInput = document.querySelector(".upload");
+    const progressBar = document.querySelector(".progress");
     const imagePreview = document.querySelector(".preview");
 
     fileInput.addEventListener("change", function(event) {
-        const file = event.target.files[0];
 
-        if (file) {
+        progressBar.style.display = "block";
+        
+        upload();
+
+        function upload() {
+            let progress = 0;
+            const interval = setInterval(function() {
+                progress += 1;
+                progressBar.value = progress;
+    
+                if (progress >= 100) {
+                    clearInterval(interval);
+                    showImage();
+
+                    progressBar.style.display = "none";
+                }
+            }, 1);
+        }
+
+        function showImage() {
+            const file = event.target.files[0];
             const imageURL = URL.createObjectURL(file);
 
             const img = document.createElement("img");
